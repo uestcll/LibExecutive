cfba7913dcdf59159e7f5068eb4fa060d3cf0741
lskxl <lilin@uestc.edu.cn>
2013-09-04 11:36:11 +0800

    temprarily save

--- include/CLMessagePoster.h
+++ include/CLMessagePoster.h
@@ -1,15 +1,26 @@
 #ifndef CLMessagePoster_H
 #define CLMessagePoster_H
 
+#include <map>
+
+class CLMessageSerializer;
+class CLProtocolEncapsulator;
+
 class CLMessagePoster
 {
 public:
-	CLMessagePoster();
+	explicit CLMessagePoster(CLProtocolEncapsulator *pProtocolEncapsulator);
+	CLMessagePoster(CLMessageSerializer *pCommonMsgSerializer, CLProtocolEncapsulator *pProtocolEncapsulator);
 	virtual ~CLMessagePoster();
 
 private:
 	CLMessagePoster(const CLMessagePoster&);
 	CLMessagePoster& operator=(const CLMessagePoster&);
+
+private:
+	CLMessageSerializer *m_pCommonSerializer;
+	std::map<unsigned long, CLMessageSerializer*> m_SerializerTable;
+	CLProtocolEncapsulator *m_pProtocolEncapsulator;
 };
 
 #endif
--- src/CLMessagePoster.cpp
+++ src/CLMessagePoster.cpp
@@ -1,9 +1,36 @@
 #include "CLMessagePoster.h"
+#include "CLMessageSerializer.h"
+#include "CLProtocolEncapsulator.h"
 
-CLMessagePoster::CLMessagePoster()
+CLMessagePoster::CLMessagePoster(CLProtocolEncapsulator *pProtocolEncapsulator)
 {
+	if(pProtocolEncapsulator == NULL)
+		throw "In CLMessagePoster::CLMessagePoster(), pProtocolEncapsulator == NULL";
+
+	m_pCommonSerializer = NULL;
+	m_pProtocolEncapsulator = pProtocolEncapsulator;
+}
+
+CLMessagePoster::CLMessagePoster(CLMessageSerializer *pCommonMsgSerializer, CLProtocolEncapsulator *pProtocolEncapsulator)
+{
+	if((pProtocolEncapsulator == NULL) ||(pCommonMsgSerializer == NULL))
+		throw "In CLMessagePoster::CLMessagePoster(), pProtocolEncapsulator == NULL or pCommonMsgSerializer == NULL";
+
+	m_pCommonSerializer = pCommonMsgSerializer;
+	m_pProtocolEncapsulator = pProtocolEncapsulator;
 }
 
 CLMessagePoster::~CLMessagePoster()
 {
+	if(m_pProtocolEncapsulator)
+		delete m_pProtocolEncapsulator;
+
+	if(m_pCommonSerializer)
+		delete m_pCommonSerializer;
+	else
+	{
+		map<unsigned long, CLMessageSerializer*>::iterator it;
+		for(it = m_SerializerTable.begin(); it != m_SerializerTable.end(); it++)
+			delete it->second;
+	}
 }
