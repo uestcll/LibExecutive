0fbecb90e41b8123361ad0b95339da053f4aa61b
lskxl <lilin@uestc.edu.cn>
2013-12-29 09:55:23 +0800

    CLProtocolDecapsulator test finished

--- test/CLProtocolDecapsulatorByDefaultMsgFormatTester.cpp
+++ test/CLProtocolDecapsulatorByDefaultMsgFormatTester.cpp
@@ -98,7 +98,7 @@ TEST(CLProtocolDecapsulatorByDefaultMsgFormat, Decapsulate_Features_Test)
 	CLStatus s3 = split.Decapsulate(IOVectorsForData, 20, vIO, IOVectorsForPartialData, &context);
 	EXPECT_TRUE(s3.IsSuccess());
 
-	iovec IOVEC[6];
+	iovec IOVEC[7];
 	IOVEC[0].iov_base = p1;
 	IOVEC[0].iov_len = 1;
 	IOVEC[1].iov_base = p2;
@@ -117,21 +117,79 @@ TEST(CLProtocolDecapsulatorByDefaultMsgFormat, Decapsulate_Features_Test)
 	EXPECT_TRUE(CheckIOVectorStatus(0, 0, IOVectorsForPartialData));
 	for(int i=0; i < vIO.size(); i++)
 		delete vIO[i];
-	/*
+
 	vIO.clear();
 	CLStatus s4 = split.Decapsulate(IOVectorsForData, 23, vIO, IOVectorsForPartialData, &context);
 	EXPECT_TRUE(s4.IsSuccess());
 	EXPECT_EQ(vIO.size(), 2);
 	EXPECT_TRUE(CheckIOVectorStatus(IOVEC, 2, *vIO[0]));
-	EXPECT_TRUE(CheckIOVectorStatus(&(IOVEC[2]), 2, *vIO[1]));
-	EXPECT_EQ(vIO.size(), 2);
-
-	IOVEC[0].iov_base = p4;
-	IOVEC[0].iov_len = 1;
-	IOVEC[1].iov_base = p5;
-	IOVEC[1].iov_len = 6;
-	EXPECT_TRUE(CheckIOVectorStatus(IOVEC, 2, IOVectorsForPartialData));
+	EXPECT_TRUE(CheckIOVectorStatus(&(IOVEC[2]), 4, *vIO[1]));
+	IOVEC[6].iov_base = p5+3;
+	IOVEC[6].iov_len = 3;
+	EXPECT_TRUE(CheckIOVectorStatus(IOVEC+6, 1, IOVectorsForPartialData));
 	for(int i=0; i < vIO.size(); i++)
 		delete vIO[i];
-*/
+}
+
+TEST(CLProtocolDecapsulatorByDefaultMsgFormat, Decapsulate2_Features_Test)
+{
+	CLProtocolDecapsulatorByDefaultMsgFormat split;
+	CLIOVectors IOVectorsForData;
+	CLIOVectors IOVectorsForPartialData;
+	vector<CLIOVectors *> vIO;
+	long context;
+
+	char *p1 = new char[1];
+	*p1 = 4;
+
+	char *p2 = new char[10];
+	int i;
+	for(i = 0; i < 10; i++)
+		p2[i] = 0;
+
+	p2[7] = 8;
+
+	char *p3 = new char[5];
+	for(i = 0; i < 5; i++)
+		p3[i] = 0;
+
+	char *p4 = new char[1];
+	*p4 = 0;
+
+	char *p5 = new char[8];
+	for(i = 0; i < 8; i++)
+		p5[i] = 0;
+	p5[3] = 10;
+
+	EXPECT_TRUE(IOVectorsForData.PushBack(p1, 1, true).IsSuccess());
+	EXPECT_TRUE(IOVectorsForData.PushBack(p2, 10, true).IsSuccess());
+	EXPECT_TRUE(IOVectorsForData.PushBack(p3, 5, true).IsSuccess());
+	EXPECT_TRUE(IOVectorsForData.PushBack(p4, 1, true).IsSuccess());
+	EXPECT_TRUE(IOVectorsForData.PushBack(p5, 8, true).IsSuccess());
+
+	CLStatus s = split.Decapsulate(IOVectorsForData, 25, vIO, IOVectorsForPartialData, &context);
+	EXPECT_TRUE(s.IsSuccess());
+
+	iovec IOVEC[7];
+	IOVEC[0].iov_base = p1;
+	IOVEC[0].iov_len = 1;
+	IOVEC[1].iov_base = p2;
+	IOVEC[1].iov_len = 7;
+	IOVEC[2].iov_base = p2+7;
+	IOVEC[2].iov_len = 3;
+	IOVEC[3].iov_base = p3;
+	IOVEC[3].iov_len = 5;
+	IOVEC[4].iov_base = p4;
+	IOVEC[4].iov_len = 1;
+	IOVEC[5].iov_base = p5;
+	IOVEC[5].iov_len = 3;
+	IOVEC[6].iov_base = p5+3;
+	IOVEC[6].iov_len = 5;
+
+	EXPECT_EQ(vIO.size(), 2);
+	EXPECT_TRUE(CheckIOVectorStatus(IOVEC, 2, *vIO[0]));
+	EXPECT_TRUE(CheckIOVectorStatus(&(IOVEC[2]), 4, *vIO[1]));
+	EXPECT_TRUE(CheckIOVectorStatus(IOVEC+6, 1, IOVectorsForPartialData));
+	for(int i=0; i < vIO.size(); i++)
+		delete vIO[i];
 }
