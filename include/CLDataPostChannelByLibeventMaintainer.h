de3b82191afd15b75369efcea8417aa2dc31ab47
lemon <lemon_wonder@163.com>
2014-03-27 16:48:35 +0800

    complete some files

--- include/CLDataPostChannelByLibeventMaintainer.h
+++ include/CLDataPostChannelByLibeventMaintainer.h
@@ -1,9 +1,22 @@
 #ifndef CLDATAPOSTCHANNELBYLIBEVENTMAINTAINER_H
 #define CLDATAPOSTCHANNELBYLIBEVENTMAINTAINER_H
 
-class CLDataPostChannelByLibeventMaintainer 
-{
+#include "CLDataPostChannelMaintainer.h"
 
+class CLDataPostChannelByLibeventMaintainer : public CLDataPostChannelMaintainer
+{
+    public:
+    CLDataPostChannelByLibeventMaintainer();
+    virtual ~CLDataPostChannelByLibeventMaintainer();
+
+    virtual CLStatus Initialize(CLInitialDataPostChannelNotifier *pNotifier, void *pContext);
+    virtual CLStatus Uninitialize(void *pContext);
+
+    virtual CLDataPoster *GetDataPoster();
+
+    private:
+    CLDataPostChannelByLibeventMaintainer(const CLDataPostChannelByLibeventMaintainer&);
+    CLDataPostChannelByLibeventMaintainer& operator=(const CLDataPostChannelByLibeventMaintainer&);
 };
 
 #endif
--- include/CLDataReceiverByLibevent.h
+++ include/CLDataReceiverByLibevent.h
@@ -1,9 +1,21 @@
 #ifndef CLDATARECEIVERBYLIBEVENT_H
 #define CLDATARECEIVERBYLIBEVENT_H
 
-class CLDataReceiverByLibevent
-{
+#include "CLDataReceiver.h"
 
+class CLDataReceiverByLibevent : public CLDataReceiver
+{
+    public:
+    CLDataReceiverByLibevent();
+    virtual ~CLDataReceiverByLibevent();
+
+    virtual CLStatus GetData(CLIOVectors& IOVectors, void *pContext);
+
+    private:
+    CLDataReceiverByLibevent(const CLDataReceiverByLibevent&);
+    CLDataReceiverByLibevent& operator=(const CLDataReceiverByLibevent&);
+
+    private:
 };
 
 
