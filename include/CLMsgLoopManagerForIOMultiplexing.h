e89b9a00077f7155568a7899f2c75569ce029ffa
lskxl <lilin@uestc.edu.cn>
2014-03-29 21:53:29 +0800

    save

--- include/CLMsgLoopManagerForIOMultiplexing.h
+++ include/CLMsgLoopManagerForIOMultiplexing.h
@@ -2,17 +2,30 @@
 #define CLMsgLoopManagerForIOMultiplexing_H
 
 #include <string>
+#include <map>
 #include "CLMessageLoopManager.h"
 
-class CLMsgLoopManagerForIOMultiplexing
+class CLMsgLoopManagerForIOMultiplexing : public CLMessageLoopManager
 {
 public:
-	CLMsgLoopManagerForIOMultiplexing();
+	CLMsgLoopManagerForIOMultiplexing(CLMessageObserver *pMsgObserver, const char* pstrThreadName);
 	virtual ~CLMsgLoopManagerForIOMultiplexing();
+
+protected:
+	virtual CLStatus Initialize();
+	virtual CLStatus Uninitialize();
+
+	virtual CLStatus WaitForMessage();
 
 private:
 	CLMsgLoopManagerForIOMultiplexing(const CLMsgLoopManagerForIOMultiplexing&);
 	CLMsgLoopManagerForIOMultiplexing& operator=(const CLMsgLoopManagerForIOMultiplexing&);
+
+private:
+	std::string m_strThreadName;
+	struct fd_set *m_pReadSet;
+	struct fd_set *m_pWriteSet;
+	//std::map<int
 };
 
 #endif
--- src/CLMsgLoopManagerForIOMultiplexing.cpp
+++ src/CLMsgLoopManagerForIOMultiplexing.cpp
@@ -1,9 +1,29 @@
+#include <string.h>
 #include "CLMsgLoopManagerForIOMultiplexing.h"
 
-CLMsgLoopManagerForIOMultiplexing::CLMsgLoopManagerForIOMultiplexing()
+CLMsgLoopManagerForIOMultiplexing::CLMsgLoopManagerForIOMultiplexing(CLMessageObserver *pMsgObserver, const char* pstrThreadName) : CLMessageLoopManager(pMsgObserver)
 {
+	if((pstrThreadName == 0) || (strlen(pstrThreadName) == 0))
+		throw "In CLMsgLoopManagerForIOMultiplexing::CLMsgLoopManagerForIOMultiplexing(), pstrThreadName error";
+
+	m_strThreadName = pstrThreadName;
 }
 
 CLMsgLoopManagerForIOMultiplexing::~CLMsgLoopManagerForIOMultiplexing()
 {
+}
+
+CLStatus CLMsgLoopManagerForIOMultiplexing::Initialize()
+{
+	return CLStatus(0, 0);
+}
+
+CLStatus CLMsgLoopManagerForIOMultiplexing::Uninitialize()
+{
+	return CLStatus(0, 0);
+}
+
+CLStatus CLMsgLoopManagerForIOMultiplexing::WaitForMessage()
+{
+	return CLStatus(0, 0);
 }
